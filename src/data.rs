87566e420c036dc25c30fdd99da520d0e134bc1d
Pedro Alves <pedromfa98@gmail.com>
2023-03-27 23:48:29 +0100

    minor changes

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,3 @@
-const SHAPE: [i32; 2] = [100, 10];
-
 #[derive(Debug)]
 pub struct NumericDataset<'a> {
     body: &'a[f64],
@@ -10,7 +8,7 @@ pub struct NumericDataset<'a> {
 impl<'a> NumericDataset<'a> {
     fn sample() -> NumericDataset<'a> {
         let body = &[1.0; 100*10];
-        let body_shape = &SHAPE;
+        let body_shape = &[100, 10];
         let target: &[u8; 100] = &[1; 100];
         
         // apply some maps to introduce some interesting patterns in the data
