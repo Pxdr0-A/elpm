d19faa3fa6b6d02542051b1fa0c127fe6914e627
Pedro Alves <pedromfa98@gmail.com>
2023-03-30 17:35:51 +0100

    very sloppy rand implement

--- src/data.rs
+++ src/data.rs
@@ -27,24 +27,42 @@ impl<'a> NumericDataset<'a> {
 }
 
 pub mod random {
-    use std::time::{SystemTime, UNIX_EPOCH};
+    use std::time::Instant;
 
     pub fn rand() {
-        let num1: Vec<u32> = vec![1, 2, 3];
-        let num2: Vec<u32> = vec![1, 2, 3];
-        let address1 = &num1 as *const Vec<u32>;
-        let address2 = &num2 as *const Vec<u32>;
-        let wait_fct1 = address1 as u32;
-        let wait_fct2 = address2 as u32;
+        let num1 = Vec::with_capacity(3);
+        let num2 = Vec::with_capacity(3);
+        let address1 = &num1 as *const Vec<u8>;
+        let address2 = &num2 as *const Vec<u8>;
+        let wait_fct1 = address1 as u128 % 37;
+        let wait_fct2 = address2 as u128 % 37;
 
-        println!("{}", wait_fct1 % 37);
-        println!("{}", wait_fct2 % 37);
-        // let inst1 = SystemTime::now();
         // do a while to simulate a coin flip
+        let mut n: u128 = 0;
+        let init_clock = Instant::now();
+        let mut new_clock: Instant = Instant::now();
+        let mut duration: u128 = 0;
+        loop {
+            n += 1;
+
+            duration = new_clock.duration_since(init_clock).as_nanos();
+            if duration < 100_000_000u128 + wait_fct1 {
+                new_clock = Instant::now();
+            } else {
+                break;
+            }
+        }
+
+        println!("{}", n);
+        prd_linear_function(&16u8, &n);
     }
 
-    fn prd_linear_function() {
-
+    fn prd_linear_function(bits: &u8, point: &u128) {
+        // define slope
+        let m: f64 = 1.0/(2u32.pow(*bits as u32) as f64);
+        let cycles = (*point as f64)/(*bits as f64);
+        println!("{}", cycles - ((cycles as u32) as f64));
+        // define period by multiples of bits
     }
 }
 
