a0a164e2c25991d4355188748be8f690259c01c2
Pedro Alves <pedromfa98@gmail.com>
2023-05-07 16:54:48 +0100

    tested new results for diferent seeds

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use crate::core::NumericDataset;
 mod core;
 mod draw;
 
-const SHAPE: [usize; 2] = [100, 2];
-const CLASSES: usize = 4;
+const SHAPE: [usize; 2] = [500, 2];
+const CLASSES: usize = 5;
+const SEED: u128 = 1;
 
 fn main() {
     // if a main thread ends, all other threads end!
     let dataset: NumericDataset;
-    dataset = NumericDataset::sample(SHAPE, CLASSES);
+    dataset = NumericDataset::sample(SHAPE, CLASSES, SEED);
 
     let (x, y, target_vec) = draw::arrange_points(&dataset, &SHAPE);
     draw::scatter_template(x, y, target_vec);
