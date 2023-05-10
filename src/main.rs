35f0d76c8263b6d020ecd1beb97ae196cecccb77
Pedro Alves <pedromfa98@gmail.com>
2023-05-10 18:33:24 +0100

    tested concurrency - working flawlessly

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ mod core;
 mod draw;
 
 use crate::core::NumericDataset;
+use crate::core::model::ElipsoideModel;
 
-const SHAPE: [usize; 2] = [500, 2];
-const CLASSES: usize = 5;
+const DIM: usize = 2;
+const SHAPE: [usize; 2] = [100, DIM];
+const CLASSES: usize = 2;
 const SEED: u128 = 1;
+
+const TOL: f64 = 0.5;
 
 fn main() {
     // if a main thread ends, all other threads end!
@@ -14,4 +18,7 @@ fn main() {
 
     let (x, y, target_vec) = draw::arrange_points(&dataset, &SHAPE);
     draw::scatter_template(x, y, target_vec);
+
+    let mut model = ElipsoideModel::new(TOL, CLASSES, DIM);
+    model.fit(dataset);
 }
