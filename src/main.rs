23df4ae6cd95eef4d8dbdd954a4f0ae41bcafa86
Pedro Alves <pedromfa98@gmail.com>
2023-05-18 00:21:28 +0100

    tested rearrangement

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@ mod core;
 mod draw;
 
 use crate::core::NumericDataset;
-use crate::core::model::ElipsoideModel;
+use crate::core::model::EllipsoidModel;
+use std::env;
 
 const DIM: usize = 2;
 const SHAPE: [usize; 2] = [100, DIM];
 const CLASSES: usize = 2;
-const SEED: u128 = 1;
+const SEED: u128 = 111;
 
 const TOL: f64 = 0.5;
 
 fn main() {
-    // if a main thread ends, all other threads end!
+    env::set_var("RUST_BACKTRACE", "1");
+
     let dataset: NumericDataset;
     dataset = NumericDataset::sample(SHAPE, CLASSES, SEED);
 
+    let mut model = EllipsoidModel::new(TOL, CLASSES, DIM);
+    model.fit(dataset.clone());
+
     let (x, y, target_vec) = draw::arrange_points(&dataset, &SHAPE);
     draw::scatter_template(x, y, target_vec);
-
-    let mut model = ElipsoideModel::new(TOL, CLASSES, DIM);
-    model.fit(dataset);
 }
